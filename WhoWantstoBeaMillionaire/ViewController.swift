ecc3396df2d1b520c9d1b79416e00c81e6abf4bb
KIrill Kopytin <kopytin-ka@mail.ru>
2021-08-27 13:38:34 +0300

    initiate commit

---
+++ WhoWantstoBeaMillionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoWantstoBeaMillionaire
+//
+//  Created by Кирилл Копытин on 27.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
